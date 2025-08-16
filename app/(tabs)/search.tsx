43b9fd5dbe272daf543888bbd07559f200748567
aqibyameen <aqibyameen644@gmail.com>
2025-08-16 05:45:40 -0700

    new features added

---
+++ app/(tabs)/search.tsx
@@ -0,0 +1,13 @@
+import { icons } from '@/constants/icons'
+import React from 'react'
+import { Image, Text, View } from 'react-native'
+
+const search = () => {
+  return (
+    <View>
+      <Text>SearchBar</Text>
+    </View>
+  )
+}
+
+export default search
